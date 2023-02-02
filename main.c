6251d94d23768bc0147a8335da5a676bea69e322
Jason Figueroa <jason.figueroa2@gmail.com>
2023-02-01 22:07:58 -0500

    added more primitives and functions

--- main.c
+++ main.c
@@ -4,8 +4,13 @@
 
 int main()
 {
-    Int a = allocIntWith(1);
-    printInt(stdout, a);
-    releasePrimitive(a);
+//    Int a = allocIntWith(1);
+//    printInt(stdout, a);
+
+    Short b = allocShortWith(32766);
+    printShort(stdout, b);
+
+//    releasePrimitive(a);
+    releasePrimitive(b);
     return 0;
 }
--- primitive.h
+++ primitive.h
@@ -3,20 +3,100 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+typedef uchar Byte;
+typedef ushort Word;
+
+// Aliases for primitives pointers, can be used in place of Object
+typedef bool *Bool;
+typedef uchar *UChar;
+typedef char *Char;
+typedef ushort *UShort;
+typedef short *Short;
+typedef uint *UInt;
 typedef int *Int;
+typedef float *Float;
+typedef ulong *ULong;
+typedef long *Long;
+typedef double *Double;
+typedef char *String;
+
+// Generic pointer
 typedef void *Object;
 
-Object
-allocInt();
+Object allocBool();
+Object allocUChar();
+Object allocChar();
+Object allocUShort();
+Object allocShort();
+Object allocInt();
+Object allocUInt();
+Object allocFloat();
+Object allocULong();
+Object allocLong();
+Object allocDouble();
+Object allocString(uint);
 
-Object
-allocIntWith(int);
+Object allocBoolWith(bool);
+Object allocUCharWith(uchar);
+Object allocCharWith(char);
+Object allocUShortWith(ushort);
+Object allocShortWith(short);
+Object allocIntWith(int);
+Object allocUIntWith(uint);
+Object allocFloatWith(float);
+Object allocULongWith(ulong);
+Object allocLongWith(long);
+Object allocDoubleWith(double);
+Object allocStringWith(String);
 
-void
-printInt(FILE *, Object);
+void copyBool(Bool, Bool);
+void copyUChar(UChar, UChar);
+void copyChar(Char, Char);
+void copyUShort(UShort, UShort);
+void copyShort(Short, Short);
+void copyInt(Int, Int);
+void copyUInt(UInt, UInt);
+void copyFloat(Float, Float);
+void copyULong(ULong, ULong);
+void copyLong(Long, Long);
+void copyDouble(Double, Double);
+void copyString(String, String);
 
-void
-releasePrimitive(Object);
+int compareBool(Object, Object);
+int compareUChar(Object, Object);
+int compareChar(Object, Object);
+int compareUShort(Object, Object);
+int compareShort(Object, Object);
+int compareInt(Object, Object);
+int compareUInt(Object, Object);
+int compareFloat(Object, Object);
+int compareULong(Object, Object);
+int compareLong(Object, Object);
+int compareDouble(Object, Object);
+int compareString(Object, Object);
+
+void printBool(FILE *, Object);
+void printUChar(FILE *, Object);
+void printChar(FILE *, Object);
+void printUShort(FILE *, Object);
+void printShort(FILE *, Object);
+void printInt(FILE *, Object);
+void printUInt(FILE *, Object);
+void printFloat(FILE *, Object);
+void printULong(FILE *, Object);
+void printLong(FILE *, Object);
+void printDouble(FILE *, Object);
+void printString(FILE *, Object);
+void printObject(FILE *, Object);
+
+void releasePrimitive(Object);
 
 #endif // PRIMITIVE_H_INCLUDED
